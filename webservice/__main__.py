28de0b78b4392f5a8cad38c1b0274f244dead742
Hatim Patrawala <48374376+Hatim001@users.noreply.github.com>
2023-11-15 13:34:41 -0400

    refactor: code refactored using re-facto plugin

--- webservice/__main__.py
+++ webservice/__main__.py
@@ -47,20 +47,20 @@ if __name__ == "__main__":
     if port is not None:
         port = int(port)
 
-    key0 = [] 
-    while len(key0) <10: 
-        while True: 
-            candidate = random.randint(0, 9) 
-            if candidate in key0: 
-                continue 
-            key0.append(candidate) 
-    
-    key1 = [] 
-    while len(key1) <10: 
-        while True: 
-            candidate = random.randint(10,19) 
-            if candidate in key1: 
-                continue 
-            key1.append(candidate) 
+    key0 = []
+    while len(key0) < 10:
+        while True:
+            candidate = random.randint(0, 9)
+            if candidate in key0:
+                continue
+            key0.append(candidate)
+
+    key1 = []
+    while len(key1) < 10:
+        while True:
+            candidate = random.randint(10, 19)
+            if candidate in key1:
+                continue
+            key1.append(candidate)
 
     web.run_app(app, port=port)
